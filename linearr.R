c609b31857b00123ed30804daceb3ccb8971b084
Alex Avila <manuel.avilaortiz@etu.univ-lille3.fr>
2022-10-25 10:36:57 +0200

    model: create linear regression

--- linearr.R
+++ linearr.R
@@ -7,8 +7,16 @@ summary(data$rad)
 
 # issue: should we consider it as a quantitative or as a factor ?
 
-# only one variable is considered as factor
+# only one variable is factor visualize the outliers
+# heavy tail on the right hand side of the distribution
+hist(data$medv)
+boxplot(data$medv)
 
+data$logmedv <- log(data$medv)
+par(mfrow=(c(2,1)))
+par(mfrow=(c(2,1)))
+
+# with log transformation, heavy tail in the left hand side
 
 # ----------------------------------------------------------------
 # output variable 3 data structure
@@ -19,5 +27,18 @@ summary(data$rad)
 # ----------------------------------------------------------------
 
 # ----------------------------------------------------------------
-# regression
+# simple regression model
 # ----------------------------------------------------------------
+
+# link between logmedv and lstat
+
+# graph
+# model estimation
+attach(data) # avoid reference to the dataset
+plot(logmedv~lstat)
+
+lm.fitl <- lm(logmedv~lstat) # logmedv = b_0 + b_1 * lstat
+
+# interpretation
+# residuals analysis
+# evaluation of the quality of the model
