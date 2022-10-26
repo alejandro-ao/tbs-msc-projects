8fc903d387b846df9fdbeecfaa2a45e1d19039a5
Alejandro <aavila0707@gmail.com>
2022-10-26 09:16:49 +0200

    feat: find the number of custers

--- 2022.10.26-k-means-case-study/main.R
+++ 2022.10.26-k-means-case-study/main.R
@@ -9,4 +9,21 @@ library(gridExtra)
 # import the data set
 customers <- read.csv("./data/customers.csv")
 
-View(customers)
+# keep only z-scores
+customers2 <- customers[, 9:11]
+View(customers2)
+
+# run many kmeans to see how many clusters we create
+# we get the choose the cluster that works best
+wss <- numeric(30)
+for (k in 1:30) {
+  wss[k] <- sum(kmeans(customers2, centers = k, nstart=25, iter.max=50)$withinss)
+}
+
+# visualise the no of clusters
+plot(1:30, 
+     wss, 
+     type="b",
+     xlab="Number of clusters",
+     ylab="Within sum of clusters"
+     )
