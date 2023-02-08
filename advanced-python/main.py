0fcaa95cc211487303119ca068f8f57c63d6494a
Alejandro <aavila0707@gmail.com>
2023-02-08 13:59:41 +0100

    feat: advanced python course

---
+++ advanced-python/main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+# Press ⌃R to execute it or replace it with your code.
+import streamlit as st
+
+
+def main():
+    st.write(
+        """
+        # My first app
+        Hello *world*!
+        """
+    )
+
+
+if __name__ == '__main__':
+    main()
